4763130323baeeffb21d604b3493fdad018be071
jing <chenjinghanlaura@gmail.com>
2026-01-21 20:05:02 -0800

    A1: added hello world and about me button.

--- NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/MainActivity.java
+++ NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button aboutMeButton = findViewById(R.id.btnAboutMe);
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Jinghan Chen\nchen.jinghan@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
