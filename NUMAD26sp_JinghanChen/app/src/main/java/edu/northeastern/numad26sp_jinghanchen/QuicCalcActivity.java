7dfb787ffae81ebe71f12efa9734b1b66ba99d31
jing <chenjinghanlaura@gmail.com>
2026-01-25 21:43:25 -0800

    A3: added activity QuicCalc and added event handler for button QuicCalc

--- NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/QuicCalcActivity.java
+++ NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/QuicCalcActivity.java
@@ -8,36 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.Toast;
-import android.content.Intent;
-
-
-public class MainActivity extends AppCompatActivity {
+public class QuicCalcActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quic_calc);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-    }
-
-    public void onClick(View view) {
-        int id = view.getId();
-
-        if (id == R.id.btnAboutMe) {
-            Toast.makeText(this,
-                    "Jinghan Chen\nchen.jinghan@northeastern.edu",
-                    Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.btnQuicCalc) {
-            Intent intent = new Intent(MainActivity.this, QuicCalcActivity.class);
-            startActivity(intent);
-        }
     }
 }
