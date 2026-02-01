58e111e3c5363e1cd8674b0c87bc878c61865872
jing <chenjinghanlaura@gmail.com>
2026-02-01 14:04:35 -0800

    A4: button will direct to link collector activity

--- NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/LinkCollectorActivity.java
+++ NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/LinkCollectorActivity.java
@@ -7,16 +7,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinkCollectorActivity extends AppCompatActivity {
+
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_quic_calc);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activitiy_link_collector);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linkCollect), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
