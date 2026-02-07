39bd0d58de66e8d6cd2e02417f422a56d27b8e75
jing <chenjinghanlaura@gmail.com>
2026-02-06 21:46:28 -0800

    A5: part2 implemented greatest prime feature without extra thread.

--- NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/prime_finder/PrimeFinderActivity.java
+++ NUMAD26sp_JinghanChen/app/src/main/java/edu/northeastern/numad26sp_jinghanchen/prime_finder/PrimeFinderActivity.java
@@ -13,32 +13,29 @@ import edu.northeastern.numad26sp_jinghanchen.R;
 public class PrimeFinderActivity extends AppCompatActivity {
 
     // part 1 finder: approach 1 init
-
     private Button btnStart1, btnStop1;
     private TextView tvCurrent1, tvPrime1;
-
     private Handler handler;
     private Thread thread1;
-
     private boolean running1 = false;
     private boolean stop1 = false;
-
     private int current1 = 3;
     private int prime1 = 0;
 
 
     // part 1 finder: approach 2 init
-
     private Button btnStart2, btnStop2;
     private TextView tvCurrent2, tvPrime2;
-
     private Thread thread2;
-
     private boolean running2 = false;
     private boolean stop2 = false;
-
     private int current2 = 3;
     private int prime2 = 0;
+
+    // part 2: greatest prime init
+    private Button btnFindGreatestPrime;
+    private TextView tvGreatestPrime;
+    private int greatestPrime = 0;
 
 
     @Override
@@ -49,15 +46,20 @@ public class PrimeFinderActivity extends AppCompatActivity {
         handler = new Handler(Looper.getMainLooper());
 
         // bind views
+        // part 1 finder: approach 1
         btnStart1 = findViewById(R.id.btnStart1);
         btnStop1  = findViewById(R.id.btnStop1);
         tvCurrent1 = findViewById(R.id.tvCurrent1);
         tvPrime1   = findViewById(R.id.tvPrime1);
-
+        // part 1 finder: approach 2
         btnStart2 = findViewById(R.id.btnStart2);
         btnStop2  = findViewById(R.id.btnStop2);
         tvCurrent2 = findViewById(R.id.tvCurrent2);
         tvPrime2   = findViewById(R.id.tvPrime2);
+        // part 2: greatest prime
+        btnFindGreatestPrime = findViewById(R.id.btnFindGreatestPrime);
+        tvGreatestPrime = findViewById(R.id.tvGreatestPrime);
+
 
         // restore after configuration change
         if (savedInstanceState != null) {
@@ -69,26 +71,35 @@ public class PrimeFinderActivity extends AppCompatActivity {
             running2 = savedInstanceState.getBoolean("running2");
             current2 = savedInstanceState.getInt("current2");
             prime2   = savedInstanceState.getInt("prime2");
+            // part 2: greatest prime
+            greatestPrime = savedInstanceState.getInt("greatestPrime");
         }
 
         updateText1();
         updateText2();
 
+        // handling events with click listeners
         // part 1 finder approach 1 buttons
         btnStart1.setOnClickListener(v -> startHandlerSearch());
         btnStop1.setOnClickListener(v -> stop1 = true);
-
         // part 1 finder approach 2 buttons
         btnStart2.setOnClickListener(v -> startRunOnUiSearch());
         btnStop2.setOnClickListener(v -> stop2 = true);
+        // part 2: greatest prime
+        btnFindGreatestPrime.setOnClickListener(v -> findGreatestPrime());
+
 
         // resume after configuration change
+        // part 1 finder: approach 1
         if (running1) startHandlerSearchFromCurrent();
+        // part 1 finder: approach 2
         if (running2) startRunOnUiSearchFromCurrent();
+        // part 2: greatest prime
+        if (greatestPrime > 0) findGreatestPrime();
     }
 
 
-
+    // handling configuration change
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -97,11 +108,12 @@ public class PrimeFinderActivity extends AppCompatActivity {
         outState.putBoolean("running1", running1);
         outState.putInt("current1", current1);
         outState.putInt("prime1", prime1);
-
         // part 1 finder: approach 2
         outState.putBoolean("running2", running2);
         outState.putInt("current2", current2);
         outState.putInt("prime2", prime2);
+        // part 2: greatest prime
+        outState.putInt("greatestPrime", greatestPrime);
     }
 
 
@@ -219,4 +231,11 @@ public class PrimeFinderActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    // part 2 greatest prime
+    private void findGreatestPrime() {
+        greatestPrime = Math.max(prime1, prime2);
+        tvGreatestPrime.setText("Greatest prime: " + greatestPrime);
+    }
+
 }
